5c0ee135c7976fe7d793426c150acbf4b9712864
DutchakKristina <dutchakk7@gmail.com>
2024-07-11 08:12:10 -0600

    Added dispatch

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,17 +1,22 @@
-import { FaPhoneAlt, FaUser } from "react-icons/fa";
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../../redux/contactsSlice';
+import { FaPhoneAlt, FaUser } from 'react-icons/fa';
 import css from './Contact.module.css';
 
-export default function Contact({ contact: { name, id, number }, onDelete }){
-    const handleDelete = () => {
-        onDelete(id);
-    };
-    return(
-        <li id={id} className={css.item}>
-           <div className={css.info}>
-                <p className={css.infoItem}><FaPhoneAlt /> {name}</p>
-                <p className={css.infoItem}><FaUser /> {number}</p>
-           </div>
-           <button onClick={handleDelete}>Delete</button>
-        </li>
-    )
+export default function Contact({ contact: { name, id, number } }) {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteContact(id));
+  };
+
+  return (
+    <li id={id} className={css.item}>
+      <div className={css.info}>
+        <p className={css.infoItem}><FaPhoneAlt /> {name}</p>
+        <p className={css.infoItem}><FaUser /> {number}</p>
+      </div>
+      <button onClick={handleDelete}>Delete</button>
+    </li>
+  );
 }
